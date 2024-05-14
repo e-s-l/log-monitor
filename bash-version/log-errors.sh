70dd0edaa81b5f759ae674e83aa03edf78dc3e77
e-s-l <e-sullivan-lester@proton.me>
2024-05-14 13:08:23 +0200

    getting there maybe

--- bash-version/log-errors.sh
+++ bash-version/log-errors.sh
@@ -21,6 +21,18 @@ get_config() {
 	echo "Remote Host: $REMOTE_HOST"
 	echo "Status file: $STATUS_FILE"
 	echo "Time till timeout: $TIMEOUT_TIME seconds"
+
+	USER="oper"
+	SERVER="10.0.109.38"
+
+	if [ "$1" == "nn" ]
+	then
+		echo "north FS"
+	elif [ "$1" == "ns" ]
+	then
+		echo "south FS"
+	fi
+
 }
 
 finisher() {
@@ -38,31 +50,41 @@ main() {
 	###
 	if $DEBUG; then echo; echo "main"; echo; fi
 	###
-	#
-	get_config
-	#
     # Assign positional parameters to variables (for clarity):
 	EXP=$1
 	TL=$2
+	#
+	get_config "$TL"
+	#
 	#File names:
 	EXP_LOG="${EXP}${TL}.log"
-	echo "Expect log file: ${EXP_LOG}"
+	echo "Expect log file: ${EXP_LOG}" ; echo
+	LOG_FILE="/usr2/log/$EXP_LOG"
 
 	#Log File Location...
 	#Status File Location...
 
-	if ! [ -f "$EXP_LOG" ]
+	if ssh "$USER@$SERVER" [ -f "$LOG_FILE" ]
 	then
-		echo "Log file $EXP_LOG not found!"
-		exit 2
-	else
 		# empty (or create) the status file
 		echo -n > "$STATUS_FILE"
-
 		# start tailing the log file:
-        if FIND_ERRORS="$(ssh oper@host "timeout TIMEOUT_TIME tail -f "$EXP_LOG" | egrep "ERROR">> "$STATUS_FILE"")"    # ooph
+
+		FIND_ERRORS="$(ssh "$USER@$SERVER" "timeout $TIMEOUT_TIME tail -f "$LOG_FILE" | egrep "ERROR"")"
+
+        if "$FIND_ERRORS"
         then
             echo "Monitoring Log File for errors"
+            while read -r line
+            do
+                if ! grep -Fxq "$line" deactivated_errors.txt       # grep -Fxq ...
+                then                                                # check in list of deactivated errors.
+                    "$line">> "$STATUS_FILE"
+                    ###
+					if $DEBUG; then echo; echo "ERROR!"; echo; fi
+					###
+                fi
+            done < <("$FIND_ERRORS")
         else
             # catch errors...
             exit_code=$?
@@ -73,6 +95,9 @@ main() {
                 finisher
             fi
         fi
+	else
+		echo "Log file $LOG_FILE not found!"
+		exit 2
 	fi
 
     ###
@@ -85,7 +110,7 @@ main() {
 DEBUG=false
 
 #CHECK POSITIONAL PARAMETERS:
-if [ "$#" -eq 3 ] && [ "$3" -eq "-d" ]		# recall: $# = len(#@)
+if [ "$#" -eq 3 ] && [ "$3" == "-d" ]		# recall: $# = len(#@)
 then
 	echo "Debug Mode: ON"
 	DEBUG=true
@@ -95,7 +120,7 @@ then
 	exit 1
 fi
 
-if ! [ "$2" -eq "NN" ] || ! [ "$2" -eq "NS" ]	# hmmm
+if ! ([ "$2" == "nn" ] || [ "$2" == "ns" ])	# hmmm
 then
 	echo "Are you not using NN or NS ?!"
 	exit 1
